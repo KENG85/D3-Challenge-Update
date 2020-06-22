96b82c6b8ac81ccb983e0181f68eb50ad1a34c1d
Kate Engard <kateengard@Kates-MacBook-Pro.local>
2020-06-21 19:59:31 -0700

    UGH

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+// Create an SVG wrappers.
 var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
@@ -22,40 +22,36 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("../data/data.csv")
-  .then(function(healthData) {
+d3.csv("https://github.com/KENG85/D3-Challenge-Update/blob/master/assets/data/data.csv")
+  .then(function(data) {
   	console.log()
-    // Step 1: Parse Data/Cast as numbers
-    // ==============================
-    healthData.forEach(function(data) {
-      data.poverty = +data.poverty;
-      data.povertyMoe = +data.povertyMoe;
-      data.age = +data.age;
-      data.ageMoe = +data.ageMoe;
-      data.income = +data.income;
-      data.incomeMoe = +data.incomeMoe;
-      data.noHealthInsurance = +data.noHealthInsurance;
-      data.obesity = +data.obesity;
-      data.smokes = +data.smokes;
+    // Parse Data/Cast as numbers
+
+    data.forEach(function(d) {
+      d.poverty = +d.poverty;
+      d.age = +d.age;
+      d.income = +d.income;
+      d.obesity = +d.obesity;
+      d.smokes = +d.smokes;
     });
 
-    // Step 2: Create scale functions
-    // ==============================
+    //  scale functions
+
     var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(healthData, d => d.poverty)-1, d3.max(healthData, d => d.poverty)])
+      .domain([d3.min(data, d => d.income)-1, d3.max(data, d => d.income)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([d3.min(healthData, d => d.noHealthInsurance)-1, d3.max(healthData, d => d.noHealthInsurance)])
+      .domain([d3.min(data, d => d.obesity)-1, d3.max(data, d => d.obesity)])
       .range([height, 0]);
 
-    // Step 3: Create axis functions
-    // ==============================
+    // Create axis functions
+
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // Step 4: Append Axes to the chart
-    // ==============================
+    // Append Axes to the chart
+
     chartGroup.append("g")
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
@@ -63,50 +59,50 @@ d3.csv("../data/data.csv")
     chartGroup.append("g")
       .call(leftAxis);
 
-    // Step 5: Create Circles
-    // ==============================
+    // Create Circles
+
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(healthData)
+    .data(data)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.noHealthInsurance))
+    .attr("cx", d => xLinearScale(d.income))
+    .attr("cy", d => yLinearScale(d.obesity))
     .attr("r", "10")
-    .attr("fill", "lightblue")
+    .attr("fill", "blue")
     .attr("opacity", "0.8");
 
-     var abbrGroup = chartGroup.selectAll("label")
-    .data(healthData)
+    var abbrGroup = chartGroup.selectAll("label")
+    .data(data)
     .enter()
     .append("text")
     .text(d => d.abbr)
     .attr("font-size",9)
     .attr("font-weight","bold")
     .attr("fill", "white")
-    .attr("x", d => xLinearScale(d.poverty)-7)
-    .attr("y", d => yLinearScale(d.noHealthInsurance)+4);
+    .attr("x", d => xLinearScale(d.income)-7)
+    .attr("y", d => yLinearScale(d.obesity)+4);
    
-    // Step 6: Initialize tool tip
-    // ==============================
+    //  tool tip
+
     var toolTip = d3.tip()
       .attr("class", "tooltip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.abbr}<br>In Poverty: ${d.poverty}%<br>No Healthcare: ${d.noHealthInsurance}%`);
+        return (`${d.abbr}<br>Income: ${d.income}%<br> Obesity: ${d.obesity}%`);
       });
 
-    // Step 7: Create tooltip in the chart
-    // ==============================
+    // call tooltip in the chart
+
     chartGroup.call(toolTip);
 
-    // Step 8: Create event listeners to display and hide the tooltip
-    // ==============================
-    abbrGroup.on("mouseover", function(data) {
-      toolTip.show(data, this);
+    // Create event listeners to display and hide the tooltip
+
+    abbrGroup.on("mouseover", function(d) {
+      toolTip.show(d, this);
     })
       // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
+      .on("mouseout", function(d, index) {
+        toolTip.hide(d);
       });
 
     // Create axes labels
@@ -116,10 +112,10 @@ d3.csv("../data/data.csv")
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Lacks Healthcare (%)");
+      .text("Obesity (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("In Poverty (%)");
+      .text("Income ($USD)");
   });
