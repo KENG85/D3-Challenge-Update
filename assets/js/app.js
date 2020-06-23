30ec0ec7713a9e2605507688b722078c86f69410
Kate Engard <kateengard@Kates-MBP.PK5001Z>
2020-06-23 13:19:33 -0700

    new

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,28 +22,25 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("./data/data.csv")
+d3.csv("https://raw.github.com/KENG85/D3-Challenge-Update/master/assets/data/data.csv")
   .then(function(data) {
-  	console.log()
+  	console.log(data)
     // Parse Data/Cast as numbers
 
     data.forEach(function(d) {
-      d.state = d.state;
+      d.abbr = d.abbr;
       d.poverty = +d.poverty;
-      d.age = +d.age;
-      d.income = +d.income;
-      d.obesity = +d.obesity;
-      d.smokes = +d.smokes;
+      d.healthcare = +d.healthcare;
     });
 
     //  scale functions
 
     var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(data, d => d.income)-1, d3.max(data, d => d.income)])
+      .domain([d3.min(data, d => d.poverty)-1, d3.max(data, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([d3.min(data, d => d.obesity)-1, d3.max(data, d => d.obesity)])
+      .domain([d3.min(data, d => d.healthcare)-1, d3.max(data, d => d.healthcare)])
       .range([height, 0]);
 
     // Create axis functions
@@ -62,9 +59,8 @@ d3.csv("./data/data.csv")
 
     // Create Circles
 
-    var circlesGroup = chartGroup.selectAll("circle")
-    .data(data)
-    .enter()
+    var circlesGroup = chartGroup.selectAll("circle").data(data).enter()
+    circlesGroup
     .append("circle")
     .attr("cx", d => xLinearScale(d.income))
     .attr("cy", d => yLinearScale(d.obesity))
@@ -72,16 +68,33 @@ d3.csv("./data/data.csv")
     .attr("fill", "blue")
     .attr("opacity", "0.8");
 
-    var labelGroup = chartGroup.selectAll("label")
-    .data(data)
-    .enter()
-    .append("text")
-    .text(d => d.state)
-    .attr("font-size",9)
-    .attr("font-weight","bold")
-    .attr("fill", "white")
-    .attr("x", d => xLinearScale(d.income)-5)
-    .attr("y", d => yLinearScale(d.obesity)+5);
+    circlesGroup.append("text").text(function(d){
+      return d.abbr;
+    }).attr("dx", d => xLinearScale(d.poverty))
+    .attr("dy", d => yLinearScale(d.healthcare)+10/2.5)
+    .attr("font-size","9")
+    .attr("class","stateText")
+    .on("mouseover", function(data, index) {
+      
+      toolTip.show(data,this);
+    d3.select(this).style("stroke","#323232")
+    .style("stroke-width","10")
+    })
+    .on("mouseout", function(data, index) {
+        toolTip.hide(data,this)
+     d3.select(this).style("stroke","#e3e3e3")
+    });
+
+    // var labelGroup = chartGroup.selectAll("label")
+    // // .data(data)
+    // .enter()
+    // .append("text")
+    // .text(d => d.abbr)
+    // .attr("font-size",9)
+    // .attr("font-weight","bold")
+    // .attr("fill", "white")
+    // .attr("x", d => xLinearScale(d.income)-5)
+    // .attr("y", d => yLinearScale(d.obesity)+5);
    
     //  tool tip
 
@@ -89,7 +102,7 @@ d3.csv("./data/data.csv")
       .attr("class", "tooltip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.state}<br>Income: ${d.income}%<br> Obesity: ${d.obesity}%`);
+        return (`${d.state}<br>Poverty: ${d.poverty}%<br> Healthcare: ${d.healthcare}%`);
       });
 
     // call tooltip in the chart
@@ -98,13 +111,13 @@ d3.csv("./data/data.csv")
 
     // Create event listeners to display and hide the tooltip
 
-    labelGroup.on("mouseover", function(d) {
-      toolTip.show(d, this);
-    })
-      // onmouseout event
-      .on("mouseout", function(d, index) {
-        toolTip.hide(d);
-      });
+    // labelGroup.on("mouseover", function(d) {
+    //   toolTip.show(d, this);
+    // })
+    //   // onmouseout event
+    //   .on("mouseout", function(d, index) {
+    //     toolTip.hide(d);
+    //   });
 
     // Create axes labels
     chartGroup.append("text")
@@ -113,10 +126,10 @@ d3.csv("./data/data.csv")
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Obesity (%)");
+      .text("Healthcare");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Income ($USD)");
+      .text("Poverty");
   });
